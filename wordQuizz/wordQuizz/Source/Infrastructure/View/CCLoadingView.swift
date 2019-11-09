ee9cfabf7362fb47a9b3be656a8f583de91a74a4
fontrap <raphael.fontes-silva@itau-unibanco.com.br>
2019-11-09 00:36:31 -0300

    created layout/view for quiz screen

---
+++ wordQuizz/wordQuizz/Source/Infrastructure/View/CCLoadingView.swift
@@ -0,0 +1,14 @@
+//
+//  CCLoadingView.swift
+//  wordQuizz
+//
+//  Created by Raphael Henrique Fontes Sil on 08/11/19.
+//  Copyright © 2019 Raphael Henrique Fontes Sil. All rights reserved.
+//
+
+import UIKit
+
+class CCLoadingView: UIView {
+    
+    
+}
