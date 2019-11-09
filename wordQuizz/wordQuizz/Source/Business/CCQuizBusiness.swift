6fb63a38b47a70cc10a3eb8cb695bcfa3ad6f899
fontrap <raphael.fontes-silva@itau-unibanco.com.br>
2019-11-08 21:42:33 -0300

    initial drawing of the architecture

---
+++ wordQuizz/wordQuizz/Source/Business/CCQuizBusiness.swift
@@ -0,0 +1,9 @@
+//
+//  CCQuizBusiness.swift
+//  wordQuizz
+//
+//  Created by Raphael Henrique Fontes Sil on 08/11/19.
+//  Copyright © 2019 Raphael Henrique Fontes Sil. All rights reserved.
+//
+
+import Foundation
---
+++ wordQuizz/wordQuizz/Source/Presentation/ViewModel/CCQuizViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CCQuizViewModel.swift
+//  wordQuizz
+//
+//  Created by Raphael Henrique Fontes Sil on 08/11/19.
+//  Copyright © 2019 Raphael Henrique Fontes Sil. All rights reserved.
+//
+
+import Foundation
+
+// Probably not going to be used
+
